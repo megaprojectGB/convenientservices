fa6836fd6b2bed17ee541bc29982519e1dc524e1
Litvak Ivan <litvakivan@gmail.com>
2021-11-05 19:33:24 +0300

    Коммит 3 - Создан дизайн страница регистрации - Создан дизайн страница авторизации - Css - logo - удален пробел в sql скрипте

--- src/main/resources/db/migration/V2__adduser.sql
+++ src/main/resources/db/migration/V2__adduser.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, first_name,  last_name, password, is_activated, email, phone)
+INSERT INTO users (username, first_name, last_name, password, is_activated, email, phone)
 VALUES ('user', 'Jean', 'Billy', '$2a$10$iKNz2CivqvKERpJ9NvhcQetpSc0QnIj58t6szc8pk2GaA8gZcMCnW', 'true', 'info@mail.ru', '79065665656');
 
 INSERT INTO roles (name)
