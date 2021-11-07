3a9b52ea73e210a7f46eff3d5d6bcf4ccbfcf4c2
Tohanos <kravchenko_ae@mail.ru>
2021-11-07 19:41:54 +0300

    SQL scripts for database generation

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,31 +1,122 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users
-(
-    id              SERIAL PRIMARY KEY,
-    username        VARCHAR(255) UNIQUE,
-    first_name      VARCHAR(255),
-    last_name       VARCHAR(255),
-    password        VARCHAR(255),
-    is_archived     BOOLEAN DEFAULT FALSE,
-    is_activated    BOOLEAN DEFAULT FALSE,
-    activation_code VARCHAR(255),
-    email           VARCHAR(255) UNIQUE,
-    phone           VARCHAR(255) UNIQUE
+CREATE TABLE "users" (
+                         "id" SERIAL PRIMARY KEY,
+                         "username" VARCHAR(255) UNIQUE,
+                         "first_name" VARCHAR(255),
+                         "last_name" VARCHAR(255),
+                         "password" VARCHAR(255),
+                         "is_archived" BOOLEAN DEFAULT FALSE,
+                         "is_activated" BOOLEAN DEFAULT FALSE,
+                         "activation_code" VARCHAR(255),
+                         "email" VARCHAR(255) UNIQUE,
+                         "phone" VARCHAR(255) UNIQUE
 );
 
-DROP TABLE IF EXISTS roles;
-CREATE TABLE roles
-(
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+CREATE TABLE "roles" (
+                         "id" SERIAL PRIMARY KEY,
+                         "name" VARCHAR(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS users_roles;
-CREATE TABLE users_roles
-(
-    user_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
-    PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) references users (id),
-    FOREIGN KEY (role_id) references roles (id)
+CREATE TABLE "users_roles" (
+                               "user_id" BIGINT NOT NULL,
+                               "role_id" BIGINT NOT NULL,
+                               PRIMARY KEY ("user_id", "role_id")
 );
+
+CREATE TABLE "addresses" (
+                             "id" SERIAL PRIMARY KEY,
+                             "zipcode" VARCHAR(255),
+                             "cityId" int,
+                             "address1" VARCHAR(255),
+                             "address2" VARCHAR(255)
+);
+
+CREATE TABLE "cities" (
+                          "id" int PRIMARY KEY,
+                          "name" VARCHAR(30),
+                          "state" VARCHAR(30),
+                          "country" VARCHAR(2)
+);
+
+CREATE TABLE "serviceCategory" (
+                                   "id" SERIAL PRIMARY KEY,
+                                   "name" VARCHAR(30),
+                                   "description" VARCHAR(255)
+);
+
+CREATE TABLE "office" (
+                          "id" SERIAL PRIMARY KEY,
+                          "addressId" int,
+                          "categoryId" int
+);
+
+CREATE TABLE "category" (
+                            "id" SERIAL PRIMARY KEY,
+                            "name" VARCHAR(255),
+                            "description" VARCHAR(255)
+);
+
+CREATE TABLE "service" (
+                           "id" SERIAL PRIMARY KEY,
+                           "serviceCategoryId" int,
+                           "name" VARCHAR(255),
+                           "duration" interval
+);
+
+CREATE TABLE "pointOfServices" (
+                                   "id" SERIAL PRIMARY KEY,
+                                   "name" VARCHAR(255),
+                                   "bossUserId" int,
+                                   "addressId" int
+);
+
+CREATE TABLE "master_pos" (
+                              "masterUserId" int,
+                              "pointOfServicesId" int
+);
+
+CREATE TABLE "master_serviceCategory" (
+                                          "masterUserId" int,
+                                          "serviceCategoryId" int
+);
+
+CREATE TABLE "booking" (
+                           "id" SERIAL PRIMARY KEY,
+                           "userId" int,
+                           "masterId" int,
+                           "pointOfServicesId" int,
+                           "dt" timestamp,
+                           "serviceId" int
+);
+
+ALTER TABLE "users_roles" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "users_roles" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("id");
+
+ALTER TABLE "addresses" ADD FOREIGN KEY ("cityId") REFERENCES "cities" ("id");
+
+ALTER TABLE "office" ADD FOREIGN KEY ("addressId") REFERENCES "addresses" ("id");
+
+ALTER TABLE "office" ADD FOREIGN KEY ("categoryId") REFERENCES "category" ("id");
+
+ALTER TABLE "service" ADD FOREIGN KEY ("serviceCategoryId") REFERENCES "serviceCategory" ("id");
+
+ALTER TABLE "pointOfServices" ADD FOREIGN KEY ("bossUserId") REFERENCES "users" ("id");
+
+ALTER TABLE "pointOfServices" ADD FOREIGN KEY ("addressId") REFERENCES "addresses" ("id");
+
+ALTER TABLE "master_pos" ADD FOREIGN KEY ("masterUserId") REFERENCES "users" ("id");
+
+ALTER TABLE "master_pos" ADD FOREIGN KEY ("pointOfServicesId") REFERENCES "pointOfServices" ("id");
+
+ALTER TABLE "master_serviceCategory" ADD FOREIGN KEY ("masterUserId") REFERENCES "users" ("id");
+
+ALTER TABLE "master_serviceCategory" ADD FOREIGN KEY ("serviceCategoryId") REFERENCES "serviceCategory" ("id");
+
+ALTER TABLE "booking" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+
+ALTER TABLE "booking" ADD FOREIGN KEY ("masterId") REFERENCES "users" ("id");
+
+ALTER TABLE "booking" ADD FOREIGN KEY ("pointOfServicesId") REFERENCES "pointOfServices" ("id");
+
+ALTER TABLE "booking" ADD FOREIGN KEY ("serviceId") REFERENCES "service" ("id");
+
---
+++ src/test/java/com/convenientservices/web/jpa/RoleDataJpaTest.java
@@ -0,0 +1,12 @@
+package com.convenientservices.web.jpa;
+
+//import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoleDataJpaTest {
+//    private static Logger logger = LoggerFactory.getLogger(RoleDataJpaTest.class);
+//
+//    @Before
+
+}
