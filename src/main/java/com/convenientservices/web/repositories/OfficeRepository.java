6bb8fd45d29f57e76fbf27023dcf25ad7905cd0d
Maverickspb78 <Maverickspb78@gmail.com>
2021-11-18 16:23:17 +0300

    скрипт тестовых данных в БД

--- src/main/java/com/convenientservices/web/repositories/OfficeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.convenientservices.web.repositories;
-
-import com.convenientservices.web.entities.Office;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfficeRepository extends JpaRepository<Office, Long> {
-}
