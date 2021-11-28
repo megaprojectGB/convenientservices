f99989fa7947deddda18cd6aa80ed44ca20b2d59
Tohanos <kravchenko_ae@mail.ru>
2021-11-28 09:04:44 +0300

    Added exception classes

---
+++ src/main/java/com/convenientservices/web/Exceptions/RecordNotFoundException.java
@@ -0,0 +1,7 @@
+package com.convenientservices.web.Exceptions;
+
+public class RecordNotFoundException extends Exception{
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+}
