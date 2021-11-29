a0430a7c3fc21a5d49cbaa1bc47e84a8210d7549
Anton Kravchenko <34935162+Tohanos@users.noreply.github.com>
2021-11-29 22:05:18 +0300

    Delete src/main/java/com/convenientservices/web/Exceptions directory

--- src/main/java/com/convenientservices/web/Exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.convenientservices.web.Exceptions;
-
-public class UserNotFoundException extends RecordNotFoundException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
